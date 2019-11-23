bc104b31a7be05c726fd21a95ee524fb69d41fa2
wh <1281947642@qq.com>
2019-11-23 15:15:51 +0800

    VUE

---
+++ hello-vue/src/js/register.js
@@ -0,0 +1,11 @@
+const registerForm = {  //组件内的template只能有一个根标签
+    template:` 
+    <div>
+        <h1>注册页</h1>
+        用&ensp;户&ensp;名:<input type="text"><br>
+        密&emsp;&emsp;码:<input type="password"><br>
+        确定密码:<input type="password"><br>
+        <input type="button" value="注册">
+    </div>
+    `
+}
